f279662bb07bfe78485a83efc57be7d913cebe42
apiwathantrakool <nekky.apiwat@gmail.com>
2022-11-03 21:23:57 +0700

    merge branch data structure to main

---
+++ datastructure/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	person01 := person{firstName: "Alex", lastName: "Anderson", contact: contactInfo{
+		email:   "alex@test.com",
+		zipCode: 1234,
+	}}
+	person01.updateName("Hello")
+	person01.print()
+}
+
+func (p *person) updateName(newData string) { // *pointer = Give me the value this memory address is pointing at.
+	(*p).firstName = newData
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
